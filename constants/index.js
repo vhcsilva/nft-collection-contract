44a349653916197d64a35f483999efc395fb996e
Vitor Hugo <vhcsilvaa@gmail.com>
2022-02-28 22:36:39 -0300

    changing the metadata uri

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = '0x5358B31019F14A84B1eF3eBcf4F554C395bbAA21';
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-collection-app-mauve.vercel.app//api/';
 
 module.exports = {
     WHITELIST_CONTRACT_ADDRESS,
